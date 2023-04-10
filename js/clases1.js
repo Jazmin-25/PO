7a5c8f67dc50e77452b0bacfa0cdabe5e5acfb49
Jaz <jaz.islasg@gmail.com>
2023-04-10 13:07:23 -0600

    Se realizan prácticas con po

---
+++ js/clases1.js
@@ -0,0 +1,4 @@
+const persona = {firstName: "Jane", lastName: "Doe", email: "jain.doe@gmail.com"}; //se creo objeto
+const persona1 = {firstName: "John", lastName: "Doe", email: "jdoe.doe@gmail.com", age: 40};
+console.log(persona.firstName, persona.lastName);//forma simple de crear el objeto
+console.log(persona1.firstName, persona1.lastName, persona1.age);
