bac6b28f2b7ee507dc77e285884a80bdae7f4430
Jaz <jaz.islasg@gmail.com>
2023-04-11 11:14:03 -0600

    Se realiza el departamento y salario

--- js/main.js
+++ js/main.js
@@ -3,14 +3,28 @@ class Persona {
     email= "";
     age=0;
     resume="";
-    printInfo(){
+    constructor(name, email, age, resume){
+        this.name=name;
+        this.email=email.toLowerCase(); //para correos en minusculas
+        this.age= (age<18)?18: age;
+        this.resume= resume;
+    }//constructor
+    printInfo(){// metodo
         console.log(this.name, this.email, this.age, this.resume);
-    }
-}// las clases para identificarlo con la primera en mayuscula y objetos en minuscula
+    }//printInfo
+}// las clases para identificarlo con la primera en mayuscula y objetos en minuscula// class persona
 
-let john = new Persona();
-john.name ="John Doe";
-john.age = 40;
-john.email = "jdoe@gmail.com";
+class Employee extends Persona{
+    departamento ="";
+    salario = 0.0;
+    constructor(name, email, age, resume, departamento, salario){
+        super(name, email, age, resume);
+        this.departamento=departamento;
+        this.salario= this.salario;
+    }//constructor 
+}// class Employee
 
-john.printInfo();
+let maritere = new Employee("Maritere Hernandez", "maritere@gmail.com", 21, "Java FullStack Developer", "IT", 1255.20);
+let dora = new Employee("Dora Garcia", "dorag@gmail.com", 26, "FullStack Developer", "Development", 1115.25);
+maritere.printInfo();
+dora.printInfo();
